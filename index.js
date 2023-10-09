982f22c65f511f2e476dfedf1e0c72e3a23b2b6d
jyot4 <jyotivyass2000@gmail.com>
2023-10-09 22:09:36 +0530

    first commit

--- index.js
+++ index.js
@@ -9,7 +9,14 @@ import Dashboard from "./Routes/Dashboard.js"
 const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(cors({ origin: "http://localhost:3000" }))
+// app.use(cors({ origin: "http://localhost:3000" }))
+
+app.use(cors({
+    "origin": "https://book-app-alxy.onrender.com",
+    "methods": "GET,HEAD,PUT,PATCH,POST,DELETE",
+    "preflightContinue": false,
+    "optionsSuccessStatus": 204
+   })); 
 
 app.use( user);
 app.use( books);
