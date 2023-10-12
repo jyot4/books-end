c5cafe4b9a0a7ed9c5f95fcfd9155d31be3dfee4
jyot4 <jyotivyass2000@gmail.com>
2023-10-12 10:20:22 +0530

    hrk

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ import Dashboard from "./Routes/Dashboard.js"
 const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-// app.use(cors({ origin: "http://localhost:3001" }))
+// app.use(cors({ origin: "http://localhost:3000" }))
 
 app.use(cors({
     "origin": "https://book-app-alxy.onrender.com",
